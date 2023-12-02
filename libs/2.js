a9c0d71aac60cb65b3747723b2b887ae2d24a7e4
bunglegrind <bunglegrind@libero.it>
2023-12-02 17:50:49 +0100

    Day #2 completed

--- libs/2.js
+++ libs/2.js
@@ -1,8 +1,90 @@
 import * as R from "ramda";
 
-const parseLine = R.identity;
+const input = {red: 12, green: 13, blue: 14};
+
+const parseId = R.pipe(
+    R.match(/(\d+):/),
+    R.prop(1),
+    Number
+);
+
+const parseGame = R.pipe(
+    R.split(","),
+    R.map(R.match(/(\d+) (\w+)/)),
+    function (v) {
+        const obj = {};
+        v.forEach(function (cubes) {
+            obj[cubes[2]] = Number(cubes[1]);
+        });
+
+        return obj;
+    }
+);
+
+const parseLine = function (line) {
+    const obj = {};
+    obj.id = parseId(line);
+    obj.grabs = R.pipe(
+        R.split(";"),
+        R.map(parseGame)
+    )(line);
+
+    return obj;
+}
+
+const isCompatible = R.pipe(
+    parseLine,
+    (game) => (
+        R.all((extraction) => R.all(
+            (color) => extraction[color] <= (input[color] ?? 0),
+            Object.keys(extraction)
+        ), game.grabs)
+        ? game.id
+        : 0
+    )
+);
+
+const calcCubes = R.pipe(
+    parseLine,
+    function (game) {
+        const obj = {};
+        R.forEach(function (extraction) {
+            R.forEach(function (color) {
+                obj[color] = Math.max(extraction[color], obj[color] ?? 0);
+            }, Object.keys(extraction));
+        }, game.grabs);
+        return obj;
+    }
+);
 
 export default {
     parseLine,
-    exec: {a: R.identity, b: R.identity}
+    isCompatible,
+    calcCubes,
+    exec: {
+        a: R.pipe(
+            R.split("\n"),
+            R.transduce(
+                R.compose(
+                    R.reject(R.isEmpty),
+                    R.map(isCompatible)
+                ),
+                R.add,
+                0
+            )
+        ),
+        b: R.pipe(
+            R.split("\n"),
+            R.transduce(
+                R.compose(
+                    R.reject(R.isEmpty),
+                    R.map(calcCubes),
+                    R.map(R.values),
+                    R.map(R.product)
+                ),
+                R.add,
+                0
+            )
+        )
+    }
 };
--- test/2.js
+++ test/2.js
@@ -9,16 +9,75 @@ Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
 Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
 Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green`;
 
-const input = {red: 12, green: 13, blue: 14};
 
 test("Parse line #1", function (t) {
     t.deepEqual(lib.parseLine(c.firstLine(sample)), {
         id: 1,
         grabs: [
             {blue: 3, red: 4},
-            {green: 2, red: 1},
-            {blue: 6, green: 2},
+            {green: 2, red: 1, blue: 6},
+            {green: 2},
         ]
     });
+    t.end();
+});
+
+test("First game is compatible with input", function (t) {
+    t.equal(lib.isCompatible(c.firstLine(sample)), 1);
+    t.end();
+});
+
+test("Second game is compatible with input", function (t) {
+    t.equal(lib.isCompatible(c.secondLine(sample)), 2);
+    t.end();
+});
+
+test("Third game is not compatible with input", function (t) {
+    t.equal(lib.isCompatible(c.thirdLine(sample)), 0);
+    t.end();
+});
+
+test("Fourth game is not compatible with input", function (t) {
+    t.equal(lib.isCompatible(c.fourthLine(sample)), 0);
+    t.end();
+});
+
+test("Fifth game is compatible with input", function (t) {
+    t.equal(lib.isCompatible(c.fifthLine(sample)), 5);
+    t.end();
+});
+
+test("Sample first part", function (t) {
+    t.equal(lib.exec.a(sample), 8);
+    t.end();
+});
+
+test("First game needs at least the following cubes", function (t) {
+    t.deepEqual(lib.calcCubes(c.firstLine(sample)), {red: 4, green: 2, blue: 6});
+    t.end();
+});
+
+test("Second game needs at least the following cubes", function (t) {
+    t.deepEqual(lib.calcCubes(c.secondLine(sample)), {red: 1, green: 3, blue: 4});
+    t.end();
+});
+
+test("Third game needs at least the following cubes", function (t) {
+    t.deepEqual(lib.calcCubes(c.thirdLine(sample)), {red: 20, green: 13, blue: 6});
+    t.end();
+});
+
+test("Fourth game needs at least the following cubes", function (t) {
+    t.deepEqual(lib.calcCubes(c.fourthLine(sample)), {red: 14, green: 3, blue: 15});
+    t.end();
+});
+
+test("Fifth game needs at least the following cubes", function (t) {
+    t.deepEqual(lib.calcCubes(c.fifthLine(sample)), {red: 6, green: 3, blue: 2});
+    t.end();
+});
+
+test("Sample second part", function (t) {
+    t.equal(lib.exec.b(sample), 2286);
     t.end();
 });

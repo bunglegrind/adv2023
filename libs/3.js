76e1ea39a7fac207af27dc13f690b143a3e36f80
bunglegrind <bunglegrind@libero.it>
2023-12-04 23:33:32 +0100

    Day #3 WIP

--- libs/3.js
+++ libs/3.js
@@ -1,6 +1,50 @@
 import * as R from "ramda";
 
+const getNumbers = R.pipe(
+    R.match(/(\d+)/g),
+    R.map(Number)
+);
+
+const mapIndexed = R.addIndex(R.map);
+
+function grid(data) {
+    const grid = R.pipe(
+        R.split("\n"),
+        R.map(function (row) {
+            const chars = R.split("", row);
+            let characters = [];
+            let accum = ""
+            R.forEach(function (c) {
+                if (R.test(/\d/, c)) {
+                    accum = R.concat(accum, c);
+                    return;
+                }
+                if (accum.length && R.test(/\D/, c)) {
+                    characters = R.concat(
+                        characters,
+                        R.map(function (cc) {
+                            return {value: Number(accum), char: cc};
+                        }, R.split("", accum))
+                    );
+                    accum = "";
+                }
+                if (!accum.length && R.test(/\D/, c)) {
+                    characters = R.concat(characters, {value: c});
+                    return;
+                }
+            }, chars);
+            return characters;
+        })
+    )(data);
+
+    return {};
+}
+
+const getNonPartNumbers = R.identity;
+
 export default {
+    getNonPartNumbers,
+    getNumbers,
     exec: {
         a: R.identity,
         b: R.identity
--- test/3.js
+++ test/3.js
@@ -3,3 +3,37 @@ import test from "tape";
 import c from "../libs/common.js";
 import lib from "../libs/3.js";
 
+const sample = `467..114..
+...*......
+..35..633.
+......#...
+617*......
+.....+.58.
+..592.....
+......755.
+...$.*....
+.664.598..`;
+
+test("Give me all the numbers",  function (t) {
+    t.deepEqual(lib.getNumbers(sample), [
+        467,
+        114,
+        35,
+        633,
+        617,
+        58,
+        592,
+        755,
+        664,
+        598
+    ]);
+    t.end();
+});
+
+test("Give me non part numbers",  function (t) {
+    t.deepEqual(lib.getNonPartNumbers(sample), [
+        114,
+        58
+    ]);
+    t.end();
+});

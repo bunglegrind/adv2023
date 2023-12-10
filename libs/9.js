8a44faf83ecb38c3bc9c6f44800fb21ca420e625
bunglegrind <bunglegrind@libero.it>
2023-12-10 23:44:07 +0100

    Day #9 WIP

--- libs/9.js
+++ libs/9.js
@@ -1,6 +1,13 @@
 import * as R from "ramda";
 
+const parse = R.pipe(
+    R.split("\n"),
+    R.reject(R.isEmpty),
+    R.map(R.pipe(R.split(" "), R.map(Number)))
+);
+
 export default {
+    parse,
     exec: {
         a: R.identity,
         b: R.identity
--- test/9.js
+++ test/9.js
@@ -2,10 +2,21 @@ import test from "tape";
 import lib from "../libs/9.js";
 import c from "../libs/common.js";
 
-const sample = ``;
+const sample = `0 3 6 9 12 15
+1 3 6 10 15 21
+10 13 16 21 30 45`;
+
+test("parse sample", function (t) {
+    t.deepEqual(lib.parse(sample), [
+        [0, 3, 6, 9, 12, 15],
+        [1, 3, 6, 10, 15, 21],
+        [10, 13, 16, 21, 30, 45],
+    ]);
+    t.end();
+});
 
 test.skip("Sample first part", function (t) {
-        t.equal(lib.exec.a(sample), 6);
+        t.equal(lib.exec.a(sample), 114);
         t.end();
 });
 

80fcb194d49a534428bc06fab1a59cab422c351b
bunglegrind <bunglegrind@libero.it>
2023-12-10 23:30:04 +0100

    Day #8 completed

--- libs/8.js
+++ libs/8.js
@@ -1,8 +1,100 @@
 import * as R from "ramda";
 
+
+const start = "AAA";
+const end = "ZZZ";
+
+const parse = R.pipe(
+    R.split("\n\n"),
+    R.reject(R.isEmpty),
+    R.applySpec({
+        directions: R.pipe(
+            R.head,
+            R.split(""),
+            R.filter((c) => R.flip(R.includes)("RL", c))
+        ),
+        map: R.pipe(
+            R.last,
+            R.split("\n"),
+            R.reject(R.isEmpty),
+            R.map(
+                R.pipe(
+                    R.match(/((\w+) = \((\w+), (\w+)\))/),
+                    R.tail,
+                    R.applySpec({
+                        node: R.prop(1),
+                        left: R.prop(2),
+                        right: R.prop(3)
+                    })
+                )
+            )
+        )
+    })
+);
+
+const path = function ({map, directions}) {
+    const findStart = R.filter(R.propSatisfies(R.test(/\w\wA/), "node"));
+    const isEnd = R.test(/\w\wZ/);
+
+    const findPlace = (place) => R.find(R.propEq(place, "node"), map);
+    const findPlaces = (places) => R.map(findPlace, places);
+
+    function move(place) {
+        let step = 0;
+        let count = 0;
+        let next;
+        while (next !== end) {
+            next = (
+                directions[step] === "L"
+                ? place.left
+                : place.right
+            );
+            step = (step + 1) % directions.length;
+            place = findPlace(next);
+            count += 1;
+        }
+
+        return count;
+    }
+
+    const run = () => move(findPlace(start));
+    const ghostRun = () => R.map(toEnd, findStart(map));
+
+    function toEnd(place) {
+        let step = 0;
+        let count = 0;
+        let next = "";
+        while (!isEnd(next)) {
+            next = (
+                directions[step % directions.length] === "L"
+                ? place.left
+                : place.right
+            );
+            step += 1;
+            place = findPlace(next);
+            count += 1;
+        }
+
+        return count;
+    }
+
+    return {run, ghostRun};
+};
+
 export default {
+    parse,
     exec: {
-        a: R.identity,
-        b: R.identity
+        a: R.pipe(
+            parse,
+            path,
+            R.invoker(0, "run")
+        ),
+        b: R.pipe(
+            parse,
+            path,
+            R.invoker(0, "ghostRun"),
+            //calculate HCF...wait...why???
+        )
+    }
 };
 
--- test/8.js
+++ test/8.js
@@ -2,7 +2,7 @@ import test from "tape";
 import lib from "../libs/8.js";
 import c from "../libs/common.js";
 
-const sample = `RL
+const sample1 = `RL
 
 AAA = (BBB, CCC)
 BBB = (DDD, EEE)
@@ -12,12 +12,58 @@ EEE = (EEE, EEE)
 GGG = (GGG, GGG)
 ZZZ = (ZZZ, ZZZ)`;
 
-test.skip("Sample first part", function (t) {
-    t.equal(lib.exec.a(sample), 6440);
+const sample2 = `LLR
+
+AAA = (BBB, BBB)
+BBB = (AAA, ZZZ)
+ZZZ = (ZZZ, ZZZ)`;
+
+const sample3 = `LR
+
+11A = (11B, XXX)
+11B = (XXX, 11Z)
+11Z = (11B, XXX)
+22A = (22B, XXX)
+22B = (22C, 22C)
+22C = (22Z, 22Z)
+22Z = (22B, 22B)
+XXX = (XXX, XXX)`;
+
+test("Parse sample1", function (t) {
+    t.deepEqual(lib.parse(sample1), {
+        directions: ["R", "L"],
+        map: [
+            {node: "AAA", left: "BBB", right: "CCC"},
+            {node: "BBB", left: "DDD", right: "EEE"},
+            {node: "CCC", left: "ZZZ", right: "GGG"},
+            {node: "DDD", left: "DDD", right: "DDD"},
+            {node: "EEE", left: "EEE", right: "EEE"},
+            {node: "GGG", left: "GGG", right: "GGG"},
+            {node: "ZZZ", left: "ZZZ", right: "ZZZ"},
+        ]
+    });
     t.end();
 });
 
-test.skip("Sample second part", function (t) {
-    t.equal(lib.exec.b(sample), 5905);
+test("Parse sample2", function (t) {
+    t.deepEqual(lib.parse(sample2), {
+        directions: ["L", "L", "R"],
+        map: [
+            {node: "AAA", left: "BBB", right: "BBB"},
+            {node: "BBB", left: "AAA", right: "ZZZ"},
+            {node: "ZZZ", left: "ZZZ", right: "ZZZ"},
+        ]
+    });
+    t.end();
+});
+
+test("Sample first part", function (t) {
+    t.equal(lib.exec.a(sample1), 2);
+    t.equal(lib.exec.a(sample2), 6);
+    t.end();
+});
+
+test("Sample second part", function (t) {
+    t.equal(lib.exec.b(sample3), 6);
     t.end();
 });

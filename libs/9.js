990ced618e3531de85c3c61dec0f5e1efeb73e81
bunglegrind <bunglegrind@users.noreply.github.com>
2023-12-11 20:17:04 +0100

    Update 9.js

--- libs/9.js
+++ libs/9.js
@@ -8,11 +8,22 @@ const parse = R.pipe(
 
 const isZeroArray = R.all(R.equals(0));
 
-const calculateLastNumber = R.pipe(
+const calculateDifference = R.pipe(
     R.reverse,
     R.aperture(2),
     R.map(([y, x]) => y - x)
 );
+
+const calculateLastNumber = function (seq) {
+    let differences = [];
+    let diff = calculateDifference(seq);
+    while (!isZeroArray(diff)) {
+        differences.push(diff);
+        diff = calculateDifferences(diff);
+    }
+    
+    return R.transduce(R.map(R.last), R. add, 0, differences);
+};
 
 export default {
     parse,

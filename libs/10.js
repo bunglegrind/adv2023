c33ffeb568a878576682016344fec31d815c09bf
bunglegrind <bunglegrind@libero.it>
2023-12-12 17:57:12 +0100

    day #10 WIP

--- libs/10.js
+++ libs/10.js
@@ -1,10 +1,24 @@
 import * as R from "ramda";
 
+const parse = R.pipe(
+    R.split("\n"),
+    R.reject(R.isEmpty)
+);
+const findStart = function (map) {
+    map = parse(map);
+    console.log(map);
+    const i = R.findIndex(R.includes("S"), map);
+    const j = R.indexOf("S", map);
+
+    return [i, j];
+};
+
 export default {
+    findStart,
     exec: {
-        a: R.pipe(
+        a: R.pipe(R.identity
         ),
-        b: R.pipe(
+        b: R.pipe(R.identity
         ),
     }
 };

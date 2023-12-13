b4914c346de521599ccf479243acbd41af6a6c18
bunglegrind <bunglegrind@libero.it>
2023-12-13 07:20:38 +0100

    added elapsed time

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import fs from "node:fs";
 import * as R from "ramda";
 import utils from "./utils.js";
+import process from 'node:process';
 
 const {day, phase} = utils.process(process.argv[2] || "");
 
@@ -10,7 +11,10 @@ fs.readFile(`./assets/${day}.txt`, "utf8", function (err, data) {
         return console.log(err);
     }
     import(`./libs/${day}.js`).then(function (mod) {
-        const libs = mod.default;
-        console.log(libs.exec[phase](data));
+        const start = process.hrtime.bigint();
+        const result = mod.default.exec[phase](data);
+        const end = process.hrtime.bigint();
+        console.log(result);
+        console.log(`Elapsed time ${(end - start)/1000n} us`);
     });
 });
